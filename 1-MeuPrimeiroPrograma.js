de8a61de2be45ff9d94718fe441500eef31266ad
mailsonlima-dev <76918290+mailsonlima-dev@users.noreply.github.com>
2022-08-31 21:18:57 -0300

    Criando arquivo

---
+++ 1-MeuPrimeiroPrograma.js
@@ -0,0 +1,22 @@
+//comentário
+/*comentário*/
+
+//imprimir na tela
+console.log("Meu Primeiro programa: Trabalhando com variáveis");
+
+//criando uma variável
+const idade = 33;
+
+//imprimindo variável
+console.log("imprimindo variavel: "+idade);
+
+//imprimir texto não a variável
+console.log("idade")
+
+const idadeSomada = idade + 2;
+
+//operadores matematicos: concatenação texto + soma
+console.log("Soma idade 33 + 3 = "+(idade+3))
+console.log(("Soma idade 33 + 3 = ")+(idade-3))
+console.log("Divisão da idade 33 / 3 = "+(idade/3))
+console.log("Divisão da idade 33 / 3 = "+(idade*3))
