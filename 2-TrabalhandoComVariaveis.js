8a36f588b2bd4f22083d60687e066c96bcf1ba54
mailsonlima-dev <76918290+mailsonlima-dev@users.noreply.github.com>
2022-09-10 19:46:44 -0300

    Teste case sensitive no JavaScript

--- 2-TrabalhandoComVariaveis.js
+++ 2-TrabalhandoComVariaveis.js
@@ -1,2 +1,7 @@
 //Vendo como NodeJS mostra um erro de console com letra maiuscula
 console.log("Trabalhando com variavéis")
+
+//JavaScrip é case sensitive, teste com variavel com "i" maiusculo e menusculo
+idade = 30
+Idade = 40
+console.log(idade)
