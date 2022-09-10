cf2352131431e5f0360b70f213f6af23a04c0f57
mailsonlima-dev <76918290+mailsonlima-dev@users.noreply.github.com>
2022-09-10 20:15:41 -0300

    Ajuste nome das variaveis iguais

--- 2-TrabalhandoComVariaveis.js
+++ 2-TrabalhandoComVariaveis.js
@@ -9,13 +9,13 @@ console.log(idade)
 //Declaração de variavel 3 tipos, mas temos mais 1 tipo
 
 //Declara uma variavel tanto local como global
-var Nome = "Mailson"
+var Nome1 = "Mailson"
 
 //Variavel local de escopo de bloco
-let Nome = "Luziene"
+let Nome2 = "Luziene"
 
 //Declara uma constate de escopo de bloco
-const Nome = "Lima"
+const Nome3 = "Lima"
 
 //Variavel GLOBAL - Cuidado, gera aviso de advertência
-Nome = "Oliveira"
+Nome4 = "Oliveira"
