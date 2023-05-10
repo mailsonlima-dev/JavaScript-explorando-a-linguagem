4ffbdb7458e5618c0a0ec4aea878ba7cb51ffb8a
Mailson <maylson_lymas@hotmail.com>
2023-05-10 00:20:13 -0300

    Alteração Soma Idade + 2

--- 1-MeuPrimeiroPrograma.js
+++ 1-MeuPrimeiroPrograma.js
@@ -4,7 +4,7 @@
 //imprimir na tela
 console.log("Meu Primeiro programa: Trabalhando com variáveis");
 
-//criando uma variável
+//criando uma variavel constante
 const idade = 33;
 
 //imprimindo variável
@@ -13,7 +13,9 @@ console.log("imprimindo variavel: "+idade);
 //imprimir texto não a variável
 console.log("idade")
 
+//soma e impressão da soma
 const idadeSomada = idade + 2;
+console.log("Idade Somada: 33 + 2 = "+idadeSomada)
 
 //operadores matematicos: concatenação texto + soma
 console.log("Soma idade 33 + 3 = "+(idade+3))
