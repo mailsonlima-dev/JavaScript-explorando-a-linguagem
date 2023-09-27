8d059f78fb86d73b5418a4927f8c2a2616999111
Mailson <maylson_lymas@hotmail.com>
2023-09-27 20:40:14 -0300

    Teste de string com console.log

--- 2-TrabalhandoComVariaveis.js
+++ 2-TrabalhandoComVariaveis.js
@@ -20,7 +20,16 @@ const Nome3 = "Lima"
 //Variavel GLOBAL - Cuidado, gera aviso de advertência
 Nome4 = "Oliveira"
 
-//Variavél com aspas simples
+//Variavél com aspas simples: identifica que é uma string 
 let nome4 = 'Luziene'
-
 console.log('Variável com aspas simples: '+nome4)
+
+//Utilizando aspas simples com numero vira string o valor é concatenado
+let valor1 = 5;
+let valor2 = 5;
+//mostrando variaveis com aspas simples
+console.log('Valor de soma de '+valor1+' + '+valor2+' = ')+valor1+valor2;
+console.log("Valro de soma de "+valor1+" + "+valor2+" = "+valor1+valor2);
+
+let soma = valor1 + valor2
+console.log(soma)
