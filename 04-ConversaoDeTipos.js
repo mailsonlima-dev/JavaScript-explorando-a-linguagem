80b5bf043ee04bd7dc05b09df205d947de864118
Mailson <76918290+mailsonlima-dev@users.noreply.github.com>
2023-10-29 19:38:11 -0300

    Adiconado isNaN e Number.isNanN verificar conversão de valores

--- 04-ConversaoDeTipos.js
+++ 04-ConversaoDeTipos.js
@@ -11,3 +11,18 @@ console.log("idade" * 2);
 console.log("Convertendo uma String em Inteiro")
 console.log(parseInt("2") + 2); // convertendo texto em inteiro
 console.log(parseFloat("2.5") + 2); // Convertendo texto em ponto flutuante
+
+console.log("------Verificando se é número isNaN---------");
+let Numero1 = 2;
+console.log(isNaN(Numero1));
+
+let Numero2 = "true";
+console.log(isNaN(Numero2));
+
+console.log("------------verificando se é número Number.isNaN------------");
+let Numero3 = 4;
+console.log(Number.isNaN(Numero3))
+
+let Numero4 = "true";
+console.log(Number.isNaN(Numero4));
+
