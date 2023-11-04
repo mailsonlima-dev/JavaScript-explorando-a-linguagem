bbc01a23590b2f2f69d60b683e3698e5ede7f1a4
Mailson <76918290+mailsonlima-dev@users.noreply.github.com>
2023-11-04 17:27:02 -0300

    Trabalhando com lista e adicionando itens

---
+++ 06-Listas.js
@@ -0,0 +1,31 @@
+console.log(`Trabalhando com Listas`);
+
+// Nome de Estados da federação Brasileira
+// trabalhar metodo sem listas como exemplo
+
+//Declarando os Estados
+const SaoPaulo = `São Paulo`;
+const Para = `Pará`;
+const Maranhao = `Maranhão`;
+
+//Imprimindo os Estados
+console.log(`Destinos possíveis:`);
+console.log(SaoPaulo, Para, Maranhao); //pode separar as variaveis com virgula
+
+// Criando uma lista array
+console.log(`Criando lista com Array`);
+
+//Declarando Array e atribuindo valores
+const ListaDeDestinos = new Array(
+    `São Paulo`,
+    `pará`,
+    `Maranhão`
+);
+
+console.log(`Lista de Destinos com Arry:`);
+console.log(ListaDeDestinos); //imprimindo o Array
+
+//Adicionando itens em um Array após criado
+ListaDeDestinos.push(`Tocantins`);
+
+console.log(ListaDeDestinos); //Imprimindo lista de destinos atualizada
