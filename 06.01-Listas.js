ef41aded01a33880f996b10710e6b61938fc08f9
Mailson <76918290+mailsonlima-dev@users.noreply.github.com>
2024-02-27 00:47:02 -0300

    Arquivo mostrando duas formas de fazer Array

---
+++ 06.01-Listas.js
@@ -0,0 +1,17 @@
+//Criando Array de outra formar mais simples
+
+//Forma 1: mais simples de criar um Array
+// let MeuArray1 = ["Carro","Onibus","Bicicleta","Avião"];
+// console.log(MeuArray1);
+
+
+//Forma 2: Criando array
+let MeuArray2 = new Array(
+    "Verde",
+    "Azul",
+    "Laranja",
+    "Rosa",
+    "Preto",
+    "Amarelo",
+);
+console.log(MeuArray2);
